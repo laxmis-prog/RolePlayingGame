1f9a10be323edeb5d2470b25c17e7693de38d2a1
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-01 18:04:51 +0300

    Add a single-line comment that says initialize buttons

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+// initialize buttons
 
 function goStore() {
-console.log("Going to store.");
+  console.log("Going to store.");
 }
 
-function goCave () {
-  console.log("Going to cave.")
+function goCave() {
+  console.log("Going to cave.");
 }
 
 function fightDragon() {
   console.log("Fighting dragon.");
 }
-
