961a45ad21cdbebcf779823581ac6270581736c8
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-01 17:51:07 +0300

    Create an empty function named goStore

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+
+function goStore() {
+
+}
+
