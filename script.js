62976150c89c7ce2eb30328402b8cd886571dcb1
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-04 09:29:16 +0300

    Move goTown function above goStore function. Copy paste the contents of the goStore function into the goTown function.

--- script.js
+++ script.js
@@ -24,6 +24,16 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 
+function goTown() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
+}
+
 
 function goStore() { 
   button1.innerText = "Buy 10 health (10 gold)";
@@ -51,6 +61,4 @@ function buyWeapon() {
 
 }
 
-function goTown() {
-  
-}
+
