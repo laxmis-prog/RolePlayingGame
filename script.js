91e9907967bfcde4145341214fda095078b85cd2
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-07 12:15:56 +0300

    Delete cat object and console statement.

--- script.js
+++ script.js
@@ -20,11 +20,7 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 
 const locations = [];
 
-const cat = {
-  name: "Whiskers",
-  "Number of legs": 4
-};
-console.log(cat["Number of legs"]);
+
 
 // initialize buttons
 button1.onclick = goStore;
