6f91913a1431a97458288d0a7a3b53cd78d9f48f
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-10 09:21:04 +0300

    Assign the innerText property of goldText to be the variable gold.Use the same pattern to update healthText with the health variable.

--- script.js
+++ script.js
@@ -79,6 +79,8 @@ function fightDragon() {
 function buyHealth() {
   gold = gold-=10;
   health = health += 10;
+  goldText.innerText = gold;
+  healthText.innerText = health;
 }
 
 function buyWeapon() {}
