7388ca1458b3acf533293817464a91054c23a9b2
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-14 09:48:53 +0300

    In between the two lines, use let to initialize a new variable called newWeapon and set this to equal weapons.

--- script.js
+++ script.js
@@ -110,9 +110,8 @@ function buyWeapon() {
   if (gold >= 30) {
     gold -= 30;
     currentWeaponIndex ++;
-
     goldText.innerText = gold;
-    
+    let newWeapon = weapons;
     text.innerText = "You now have a new weapon.";
   }
 }
