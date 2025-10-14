7d4d6a9fd5a55ce276a36878473e9d3e763f87e8
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-14 09:45:40 +0300

    Update the goldText element to display the new value of gold,and update the text element to display You now have a new weapon.

--- script.js
+++ script.js
@@ -110,6 +110,10 @@ function buyWeapon() {
   if (gold >= 30) {
     gold -= 30;
     currentWeaponIndex ++;
+
+    goldText.innerText = gold;
+    
+    text.innerText = "You now have a new weapon.";
   }
 }
 
