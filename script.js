93f5b0150a403178bfd4e70f3b9fb7c76dbc45d5
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-16 16:15:22 +0300

    After currentWeapon,use the concatenation operator to set text.innerText to the string.

--- script.js
+++ script.js
@@ -131,6 +131,7 @@ function sellWeapon() {
     gold += 15;
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
   }
 }
 
