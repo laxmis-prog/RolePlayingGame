8408dc634edb9fa3a0fca039c3d766b7ae7c8c4c
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-18 17:03:39 +0300

    Display the monsterStats element by updating the display property of the style property to block.

--- script.js
+++ script.js
@@ -166,6 +166,7 @@ function fightDragon() {
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = 'block';
 }
 
 function attack() {}
