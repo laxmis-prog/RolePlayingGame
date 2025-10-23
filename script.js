b5889dc6a95f365951c1979f099b9b78c2f5a02b
laxmis-prog <laxmisanuu5@gmail.com>
2025-10-23 09:41:42 +0300

    Below attack function,create an empty function named getMonsterAttackValue. It should take level as a parameter.

--- script.js
+++ script.js
@@ -222,6 +222,10 @@ function attack() {
   }
 }
 
+function getMonsterAttackValue (level) {
+
+}
+
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
